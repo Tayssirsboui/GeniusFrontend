e7ee0dfa50a6da8ccf16ba72f95cf8a9a105c3bb
bekiremna <99596512+bekiremna@users.noreply.github.com>
2025-04-13 20:58:21 +0100

    login-register-emailvalidation-Done

--- src/app/front/register-front/register-front.component.ts
+++ src/app/front/register-front/register-front.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthentificationService} from '../../services/services/authentification.service';
+import {RegistrationRequest} from '../../services/models/registration-request';
+
 
 @Component({
   selector: 'app-register-front',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-front.component.css']
 })
 export class RegisterFrontComponent {
+  registerRequest: RegistrationRequest = {email: '', nom: '', prenom: '', motDePasse: '',phoneNumber: '',roles: 'Student'};
+  errorMsg: Array<string> = [];
 
+  constructor(
+    private router: Router,
+    private authService: AuthentificationService
+  ) {
+  }
+
+  login() {
+    this.router.navigate(['login']);
+  }
+
+  register() {
+    this.errorMsg = [];
+    this.authService.register({
+      body: this.registerRequest
+    })
+      .subscribe({
+        next: () => {
+          this.router.navigate(['activate-account']);
+        },
+        error: (err) => {
+          this.errorMsg = err.error.validationErrors;
+        }
+      });
+  }
 }
